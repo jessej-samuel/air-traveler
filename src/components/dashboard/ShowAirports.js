c75a32407dad41b265c65a0da60144449552174b
jessej-samuel <1234jessej@gmail.com>
2022-09-18 09:22:48 +0530

    feat: show airports optimized for  mobile

--- src/components/dashboard/ShowAirports.js
+++ src/components/dashboard/ShowAirports.js
@@ -1,20 +1,7 @@
-import { Box, Container, Typography } from "@mui/material";
+import { AirplaneTicket } from "@mui/icons-material";
+import { Container } from "@mui/material";
 import { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
 import "./ShowAirports.css";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "name", headerName: "Name", width: 130 },
-  { field: "city", headerName: "City", width: 130 },
-  { field: "country", headerName: "Country", width: 130 },
-];
-
-const rows = [
-  { id: 1, name: "John", city: "New York", country: "USA" },
-  { id: 2, name: "Jack", city: "London", country: "UK" },
-  { id: 3, name: "Joe", city: "Paris", country: "France" },
-];
 
 const ShowAirports = () => {
   const [airports, setAirports] = useState([]);
@@ -43,41 +30,27 @@ const ShowAirports = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <Container class="container" maxWidth="xs" variant="outlined" elevation={3}>
-      {/* <h2>Airports</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Country</th>
-            <th>Count</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div class="container">
+      <h1 className="header">
+        Airports <AirplaneTicket fontSize="large" />
+      </h1>
+      <div className="airport-list-container">
+        <ul className="airport-list">
           {airports.map((airport) => (
-            <tr key={airport.id}>
-              <td>{airport.city}</td>
-              <td>{airport.country}</td>
-              <td>{airport.count}</td>
-            </tr>
+            <>
+              <li key={airport.id} className="airport-item">
+                <h2>{airport.city}</h2>
+                <p>{airport.location}</p>
+              </li>
+              <li key={airport.id} className="airport-item">
+                <h2>{airport.city}</h2>
+                <p>{airport.location}</p>
+              </li>
+            </>
           ))}
-        </tbody>
-      </table> */}
-      {/* convert to mui */}
-      <Typography variant="h4" component="h2">
-        Airports
-      </Typography>
-      <Box style={{ height: 400 }} maxWidth={"xs"}>
-        <DataGrid
-          sx={{ width: "100%" }}
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          // checkboxSelection
-        />
-      </Box>
-    </Container>
+        </ul>
+      </div>
+    </div>
   );
 };
 
